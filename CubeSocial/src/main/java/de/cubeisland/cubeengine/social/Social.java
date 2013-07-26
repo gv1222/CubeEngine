43e31093af138dae9c0d1422351c7c0cc4cb4f15
Phillip Schichtel <quick_wango@code-infection.de>
2013-07-26 14:52:21 -0500

    [Social] The basic shell

---
+++ CubeSocial/src/main/java/de/cubeisland/cubeengine/social/Social.java
@@ -0,0 +1,14 @@
+package de.cubeisland.cubeengine.social;
+
+import de.cubeisland.cubeengine.core.module.Module;
+
+public class Social extends Module
+{
+
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+}
