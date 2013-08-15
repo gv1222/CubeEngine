7b14dbe9d563d5ee8698312d81661048ca4b2ac3
totokaka <ttotokaka@gmail.com>
2013-08-15 20:09:34 +0200

    Fixing Social

--- social/src/main/java/de/cubeisland/engine/social/Social.java
+++ social/src/main/java/de/cubeisland/engine/social/Social.java
@@ -1,5 +1,26 @@
+/**
+ * This file is part of CubeEngine.
+ * CubeEngine is licensed under the GNU General Public License Version 3.
+ *
+ * CubeEngine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CubeEngine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CubeEngine.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.cubeisland.engine.social;
 
+import de.cubeisland.engine.core.command.CommandManager;
+import de.cubeisland.engine.core.command.reflected.ReflectedCommand;
+import de.cubeisland.engine.core.config.Configuration;
+import de.cubeisland.engine.core.module.Module;
 import de.cubeisland.engine.social.interactions.SocialCommand;
 import de.cubeisland.engine.social.interactions.SocialListener;
 import de.cubeisland.engine.social.interactions.SocialSubCommand;
